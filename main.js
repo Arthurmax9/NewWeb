6acdaaa0da1881b4abeb5bef21061ad90ac7f390
Arthurmax9 <130000778+Arthurmax9@users.noreply.github.com>
2023-12-27 19:52:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,56 +8,22 @@ $('.navbar-collapse ul li a').click(function() {
     $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top},1500,'easeInOutExpo');
     event.preventDefault();
     });
-  });
 
+
+    
+  });
   
+ 
   document.addEventListener('DOMContentLoaded', function () {
-    const projectList = document.querySelector('.project-list');
-    const prevButton = document.querySelector('.prev-button');
-    const nextButton = document.querySelector('.next-button');
-    const projectItems = document.querySelectorAll('.project-item');
-  
-    let currentIndex = 0;
-    const itemsPerPage = 2;
-  
-    prevButton.addEventListener('click', function () {
-      if (currentIndex > 0) {
-        currentIndex -= itemsPerPage;
-        updateCarousel();
-      }
-    });
-  
-    nextButton.addEventListener('click', function () {
-      const totalItems = projectItems.length;
-      if (currentIndex + itemsPerPage < totalItems) {
-        currentIndex += itemsPerPage;
-        updateCarousel();
-      }
-    });
-  
-    function updateCarousel() {
-      const translateValue = -currentIndex * (100 / itemsPerPage) + '%';
-      projectList.style.transform = 'translateX(' + translateValue + ')';
-    }
-  
-    projectItems.forEach((project, index) => {
-      project.addEventListener('mouseenter', function () {
-        project.classList.add('active');
+    const figures = document.querySelectorAll('.gallery figure');
+
+    figures.forEach((figure) => {
+      figure.addEventListener('mouseover', function () {
+        figure.classList.add('enlarged');
       });
-  
-      project.addEventListener('mouseleave', function () {
-        project.classList.remove('active');
-      });
-  
-      project.addEventListener('click', function () {
-        // Adicione aqui a lógica para mostrar título e descrição
-        const title = project.querySelector('.project-title').textContent;
-        const description = project.querySelector('.project-description').textContent;
-  
-        // Exemplo: exiba os dados no console
-        console.log('Title:', title);
-        console.log('Description:', description);
+
+      figure.addEventListener('mouseout', function () {
+        figure.classList.remove('enlarged');
       });
     });
   });
-  
